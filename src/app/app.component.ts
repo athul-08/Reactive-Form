25458bc0d3c64a0c635592e0b5cd50fb9f126d47
athul-08 <athulprasad0812@gmail.com>
2024-04-05 13:13:32 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component} from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ CommonModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'task';
+  bookForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+      this.bookForm = this.fb.group({
+        bookName: ['', Validators.required],
+        author: ['', Validators.required],
+        publication: [''],
+        price: ['', Validators.required],
+        genre: [''],
+        publishedDate: [''],
+        country: [''],
+        description: ['']
+      });
+    }
+
+  onSubmit() {
+    if (this.bookForm.valid) {
+      console.log(this.bookForm.value);
+    }
+  }
 }
